107bb355e2babd676877adca2bfd4981e56e6e4e
CaioEuzebio <39926679+CaioEuzebio@users.noreply.github.com>
2019-03-15 07:27:33 -0300

    Update minicalc.py

--- minicalc.py
+++ minicalc.py
@@ -1,44 +1,31 @@
+ops = [1,2,3,4]
 op = int(input("""Digite a operação: 
                     1 - Soma 
                     2 - Subtração 
                     3 - Divisão 
                     4 - Multiplicação  
                     Operação : """))
-n1 = float(input('Digite o primeiro valor: '))
-n2 = float(input('Digite o segundo valor: '))
+if op not in ops:
+    print('Operacao invalida')
+else:
+    n1 = float(input('Digite o primeiro valor: ')); n2 = float(input('Digite o segundo valor: '))
 
 
-class Soma:
+class Calc:
     def __init__(self):
         self.sum1 = n1 + n2
-somar = Soma()
-
-
-class Divisao:
-    def __init__(self):
-        self.div1 = n1 % n2
-dividir = Divisao()
-
-
-class Mult:
-    def __init__(self):
-        self.mult1 = n1 * n2
-multiplica = Mult()
-
-
-class Sub:
-    def __init__(self):
         self.subt1 = n1 - n2
-subt = Sub()
-
+        self.div1 = n1 %n2
+        self.mult1 = n1 * n2
+calcular = Calc()
 if op == 1:
-    print('o Resultado da soma é: ', somar.sum1)
+    print('o Resultado da soma é: ', calcular.sum1)
+elif op == 2:
+    print('o Resultado da Subtração é: ', calcular.subt1)
+elif op == 3:
+    print('o Resultado é Divisão: ', calcular.div1)
 else:
-    if op == 2:
-        print('o Resultado da Subtração é: ', subt.subt1)
-    else:
-        if op == 3:
-            print('o Resultado é Divisão: ', dividir.div1)
-        else:
-            if op == 4:
-                print("o Resultado é Multiplicação: ", multiplica.mult1)
+    op == 4
+    print("o Resultado é Multiplicação: ", calcular.mult1)
+
+
