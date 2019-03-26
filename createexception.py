5667072eab0c5514a553770cd31a6a076e61b61d
Caio Euzébio <39926679+CaioEuzebio@users.noreply.github.com>
2019-03-26 18:53:23 -0300

    Create createexception.py

---
+++ createexception.py
@@ -0,0 +1,21 @@
+class ValorRepetidoErro(Exception):
+    def __str__(self):
+        return 'number was typed'
+def main():
+    list = []
+
+    for i in range(3):
+        while True:
+            try:
+                num = int(input('Choose a number: '))
+                break
+            except ValueError:
+                print('type a valid num')
+        
+        if num not in list:
+            list.append(num)
+        else:
+            raise ValorRepetidoErro
+        
+if __name__ == '__main__':
+    main()
